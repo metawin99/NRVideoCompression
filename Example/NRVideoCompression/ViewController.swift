2b57a40e44016c5a05ed225a084e70f50a7192bf
metawin99 <pai.pp61@gmail.com>
2018-09-10 15:49:37 +0700

    Pai - init for swift4

--- Example/NRVideoCompression/ViewController.swift
+++ Example/NRVideoCompression/ViewController.swift
@@ -10,77 +10,116 @@ import UIKit
 import MobileCoreServices
 import NRVideoCompression
 import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
-
-    var originalVideoUrl: NSURL?
+    
     @IBOutlet weak var originalVideoSize: UILabel!
     @IBOutlet weak var newVideoSize: UILabel!
+    
+    var originalVideoUrl: NSURL?
+    var compressUrl: NSURL?
+    var moviePlayer:MPMoviePlayerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     func openImagePicker() {
         let controller = UIImagePickerController()
         //controller.allowsEditing = false
-        controller.sourceType = .Camera
+        controller.sourceType = .camera
         controller.mediaTypes = [kUTTypeMovie as String]
         controller.delegate = self
-        self.presentViewController(controller, animated: true, completion: nil)
-
+        self.present(controller, animated: true, completion: nil)
+        
     }
     
     //MARK: UIImagePickerController Delegates
-     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        picker.dismissViewControllerAnimated(true, completion: nil)
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion: nil)
         self.originalVideoUrl = info[UIImagePickerControllerMediaURL] as? NSURL
         if let _ = originalVideoUrl {
-            if NSFileManager.defaultManager().fileExistsAtPath(self.originalVideoUrl!.absoluteString) {
-                try! NSFileManager.defaultManager().removeItemAtURL(self.originalVideoUrl!)
+            if FileManager.default.fileExists(atPath: self.originalVideoUrl!.absoluteString!) {
+                try! FileManager.default.removeItem(at: self.originalVideoUrl! as URL)
             }
-
-            let originalSize = NSData(contentsOfURL: originalVideoUrl!)
-            let sizeinMB = NSByteCountFormatter.stringFromByteCount(Int64((originalSize?.length)!), countStyle: .File)
+            
+            let originalSize = NSData(contentsOf: originalVideoUrl! as URL)
+            let sizeinMB = ByteCountFormatter.string(fromByteCount: Int64((originalSize?.length)!), countStyle: .file)
             self.originalVideoSize.text = "original size\(sizeinMB)"
-
+            
             
         }
-        
     }
     
-     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        picker.dismissViewControllerAnimated(true, completion: nil)
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func selectVideoButtonClicked(sender: UIButton) {
-        
+    @IBAction func selectVideoButtonClicked(_ sender: Any) {
         openImagePicker()
     }
     
-    @IBAction func compressButtonClicked(sender: UIButton) {
-        
+    @IBAction func compressButtonClicked(_ sender: Any) {
         if let _ = self.originalVideoUrl {
-            NRVideoCompressor.compressVideoWithQuality(AVAssetExportPresetLowQuality, inputURL: originalVideoUrl!, completionHandler: { (outputUrl) in
+            //AVAssetExportPresetLowQuality
+            //AVAssetExportPresetMediumQuality
+            //AVAssetExportPresetHighestQuality
+            NRVideoCompressor.compressVideoWithQuality(presetName: AVAssetExportPresetLowQuality, inputURL: originalVideoUrl!, completionHandler: { (outputUrl) in
                 self.originalVideoUrl = nil
                 self.originalVideoSize.text = "0 MB"
-                let compressSize = NSData(contentsOfURL: outputUrl)
-                let sizeinMB = NSByteCountFormatter.stringFromByteCount(Int64((compressSize?.length)!), countStyle: .File)
-                self.newVideoSize.text = "compress size \(sizeinMB)"
-
+                let compressSize = NSData(contentsOf: outputUrl as URL)
+                let sizeinMB = ByteCountFormatter.string(fromByteCount: Int64((compressSize?.length)!), countStyle: .file)
+                self.newVideoSize.text = "compress size1 \(sizeinMB)"
+                self.compressUrl = outputUrl
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                    NRVideoCompressor.compressVideoWithQuality2(presetName: AVAssetExportPresetMediumQuality, inputURL: outputUrl, completionHandler: { (outputUrl2) in
+//                        self.originalVideoUrl = nil
+//                        self.originalVideoSize.text = "0 MB"
+//                        let compressSize = NSData(contentsOf: outputUrl2 as URL)
+//                        let sizeinMB = ByteCountFormatter.string(fromByteCount: Int64((compressSize?.length)!), countStyle: .file)
+//                        self.newVideoSize.text = "compress size2 \(sizeinMB)"
+//                        
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                            self.moviePlayer = MPMoviePlayerController(contentURL: outputUrl2 as URL?)
+//                            self.moviePlayer.view.frame = CGRect(x: 20, y: 100, width: 200, height: 150)
+//                            
+//                            self.view.addSubview(self.moviePlayer.view)
+//                            self.moviePlayer.isFullscreen = true
+//                            
+//                            self.moviePlayer.controlStyle = MPMovieControlStyle.embedded
+//                            
+//                        }
+//                    })
+//                }
+                
             })
-
         }
     }
     
-   
+    @IBAction func playVideo(_ sender: Any) {
+        if let url = compressUrl {
+            self.moviePlayer = MPMoviePlayerController(contentURL: url as URL?)
+            
+            let screenSize = UIScreen.main.bounds
+            let screenWidth = screenSize.width
+            
+            self.moviePlayer.view.frame = CGRect(x: screenWidth / 2 - 100, y: 250, width: 200, height: 200)
+            
+            self.view.addSubview(self.moviePlayer.view)
+            self.moviePlayer.isFullscreen = true
+            
+            self.moviePlayer.controlStyle = MPMovieControlStyle.embedded
+        }
+    }
     
 }
 
--- NRVideoCompression/Classes/NRVideoCompressor.swift
+++ NRVideoCompression/Classes/NRVideoCompressor.swift
@@ -21,29 +21,62 @@ public class NRVideoCompressor {
      - completionHandler: This completion handler will give you output url of compressed video.
      
      */
-    public class func compressVideoWithQuality(presetName: String, inputURL : NSURL, completionHandler:(outputUrl: NSURL) -> ()) {
+    public class func compressVideoWithQuality(presetName: String, inputURL : NSURL, completionHandler:@escaping (_ outputUrl: NSURL) -> ()) {
         
-        let videoFilePath = NSTemporaryDirectory().stringByAppendingString("/compressVideo.mp4")
+        let videoFilePath = NSTemporaryDirectory().appendingFormat("/compressVideo.mp4")
         let savePathUrl =  NSURL(fileURLWithPath: videoFilePath)
-        let sourceAsset = AVURLAsset(URL: inputURL, options: nil)
+        let sourceAsset = AVURLAsset(url: inputURL as URL, options: nil)
+        //        let sourceAsset = AVURLAsset(URL: inputURL, options: nil)
         
         let assetExport: AVAssetExportSession = AVAssetExportSession(asset: sourceAsset, presetName: presetName)!
-        assetExport.outputFileType = AVFileTypeQuickTimeMovie
-        assetExport.outputURL = savePathUrl
-        if NSFileManager.defaultManager().fileExistsAtPath(videoFilePath) {
-            try! NSFileManager.defaultManager().removeItemAtPath(videoFilePath)
+        assetExport.outputFileType = AVFileType.mov
+        assetExport.outputURL = savePathUrl as URL
+        if FileManager.default.fileExists(atPath: videoFilePath) {
+            try! FileManager.default.removeItem(atPath: videoFilePath)
         }
-        assetExport.exportAsynchronouslyWithCompletionHandler { () -> Void in
+        assetExport.exportAsynchronously { () -> Void in
             
             switch assetExport.status {
-            case AVAssetExportSessionStatus.Completed:
-                dispatch_async(dispatch_get_main_queue(), {
+            case AVAssetExportSessionStatus.completed:
+                DispatchQueue.main.async {
                     print("successfully exported at \(savePathUrl.path!))")
-                    completionHandler(outputUrl: savePathUrl)
-                })
-            case  AVAssetExportSessionStatus.Failed:
+                    completionHandler(savePathUrl)
+                }
+            case  AVAssetExportSessionStatus.failed:
                 print("failed \(assetExport.error)")
-            case AVAssetExportSessionStatus.Cancelled:
+            case AVAssetExportSessionStatus.cancelled:
+                print("cancelled \(assetExport.error)")
+            default:
+                print("complete")
+            }
+        }
+        
+    }
+    
+    public class func compressVideoWithQuality2(presetName: String, inputURL : NSURL, completionHandler:@escaping (_ outputUrl: NSURL) -> ()) {
+        
+        let videoFilePath = NSTemporaryDirectory().appendingFormat("/compressVideo2.mp4")
+        let savePathUrl =  NSURL(fileURLWithPath: videoFilePath)
+        let sourceAsset = AVURLAsset(url: inputURL as URL, options: nil)
+        //        let sourceAsset = AVURLAsset(URL: inputURL, options: nil)
+        
+        let assetExport: AVAssetExportSession = AVAssetExportSession(asset: sourceAsset, presetName: presetName)!
+        assetExport.outputFileType = AVFileType.mov
+        assetExport.outputURL = savePathUrl as URL
+        if FileManager.default.fileExists(atPath: videoFilePath) {
+            try! FileManager.default.removeItem(atPath: videoFilePath)
+        }
+        assetExport.exportAsynchronously { () -> Void in
+            
+            switch assetExport.status {
+            case AVAssetExportSessionStatus.completed:
+                DispatchQueue.main.async {
+                    print("successfully exported at \(savePathUrl.path!))")
+                    completionHandler(savePathUrl)
+                }
+            case  AVAssetExportSessionStatus.failed:
+                print("failed \(assetExport.error)")
+            case AVAssetExportSessionStatus.cancelled:
                 print("cancelled \(assetExport.error)")
             default:
                 print("complete")
